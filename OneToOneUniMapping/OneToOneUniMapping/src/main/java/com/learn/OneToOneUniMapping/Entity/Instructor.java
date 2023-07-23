98dcbc8f0c38b0cf433ef3756b4de6ab2a74f312
yajuvendra0404 <yajuvendra0404@gmail.com>
2023-07-23 11:32:32 -0400

    Entity Created

---
+++ OneToOneUniMapping/OneToOneUniMapping/src/main/java/com/learn/OneToOneUniMapping/Entity/Instructor.java
@@ -0,0 +1,107 @@
+package com.learn.OneToOneUniMapping.Entity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="instructor")
+public class Instructor {
+
+	
+	/* you don't need to create an
+	 * argument in the constructor 
+	 * for the "Auto-Generated" field*/
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name ="id")
+	private int id;
+
+
+	@Column(name ="first_name")
+	private String firstName;
+	
+	@Column(name ="last_name")
+	private String lastName;
+	
+	@Column(name ="email")
+	private String email;
+
+
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name="instructor_detail_id")
+	private InstructorDetails instructorDetails;
+	
+	
+	public Instructor(String firstName, String lastName, String email) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+
+	public String getLastName() {
+		return lastName;
+	}
+
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public InstructorDetails getInstructorDetails() {
+		return instructorDetails;
+	}
+
+
+	public void setInstructorDetails(InstructorDetails instructorDetails) {
+		this.instructorDetails = instructorDetails;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Instructor [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
+				+ ", instructorDetails=" + instructorDetails + "]";
+	}
+	
+}
