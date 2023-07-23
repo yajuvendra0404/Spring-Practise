ec70a1212b838f078e6050874b16764c15f54505
yajuvendra0404 <yajuvendra0404@gmail.com>
2023-07-23 10:01:35 -0400

    Hibernate one to one mapping

---
+++ OneToOneUniMapping/OneToOneUniMapping/src/main/java/com/learn/OneToOneUniMapping/OneToOneUniMappingApplication.java
@@ -0,0 +1,13 @@
+package com.learn.OneToOneUniMapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneToOneUniMappingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneToOneUniMappingApplication.class, args);
+	}
+
+}
---
+++ OneToOneUniMapping/OneToOneUniMapping/src/test/java/com/learn/OneToOneUniMapping/OneToOneUniMappingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.OneToOneUniMapping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneToOneUniMappingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
