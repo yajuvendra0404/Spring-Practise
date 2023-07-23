bcd7b9a0b5ddfd3474248c352075a4c73b0d6f76
yajuvendra0404 <yajuvendra0404@gmail.com>
2023-07-23 12:51:23 -0400

    one to one uni mapping done

--- OneToOneUniMapping/OneToOneUniMapping/src/main/java/com/learn/OneToOneUniMapping/OneToOneUniMappingApplication.java
+++ OneToOneUniMapping/OneToOneUniMapping/src/main/java/com/learn/OneToOneUniMapping/OneToOneUniMappingApplication.java
@@ -5,6 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import com.learn.OneToOneUniMapping.DAO.AppDAOImpl;
+import com.learn.OneToOneUniMapping.Entity.Instructor;
+import com.learn.OneToOneUniMapping.Entity.InstructorDetails;
+
 @SpringBootApplication
 public class OneToOneUniMappingApplication {
 
@@ -12,10 +16,34 @@ public class OneToOneUniMappingApplication {
 		SpringApplication.run(OneToOneUniMappingApplication.class, args);
 	}
 
+	/*
+	 * Spring will inject "AppDAOImpl" automatically 
+	 * no need to inject or use @Autowired annotation
+	*/
+	
 	@Bean
-	public CommandLineRunner commandLineRunner (String srgs[]) {
+	public CommandLineRunner commandLineRunner (AppDAOImpl appDAOImpl) {
 		return runner -> {
-			System.out.println("hellow");
+			this.createInstructor(appDAOImpl);
 		};
+	}
+	
+	public void createInstructor(AppDAOImpl appDAOImpl) {
+	
+		/*
+		 * "instructor" entity has the FK for "instructordetails"
+		 * so when you call instructor's "setInstructorDetails()" method 
+		 * data for "instructordetails" will also be saved.
+		 * first the data for "instructordetails" will be saved and then
+		 * data for "instructor" will be saved
+		*/
+		Instructor instructor = new Instructor("tim","bits","tim@bits.com");
+
+		InstructorDetails instructordetails = new InstructorDetails("www.youtube.com", "dance");
+		
+		instructor.setInstructorDetails(instructordetails);
+		
+		appDAOImpl.save(instructor);
+
 	}
 }
---
+++ OneToOneUniMapping/OneToOneUniMapping/src/main/java/com/learn/OneToOneUniMapping/Service/AppService.java
@@ -0,0 +1,8 @@
+package com.learn.OneToOneUniMapping.Service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AppService {
+
+}
