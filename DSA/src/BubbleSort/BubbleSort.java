bce06ff24250fe15ab00f63b3d0c7e7a550c68d5
yajuvendra0404 <yajuvendra0404@gmail.com>
2023-08-02 14:16:53 -0400

    bubble sort completed

--- DSA/src/BubbleSort/BubbleSort.java
+++ DSA/src/BubbleSort/BubbleSort.java
@@ -14,21 +14,22 @@ public class BubbleSort {
 		 * If there are "n" elements then there will be "n-1" comparisons in case of bubble sort. 
 		 */
 		
-		int array[] = {8,3,6,7,9,1};
-		
-		for(int i = 0; i < array.length-1; i++) { 
+		int array[] = {7,3,3,2,4,1,0};
+		boolean isSwapped;
+		for(int i = 0; i < array.length-1; i++) { // Time complexity O(n) - order of "n"
 			// This for loop is for passes which is (array.length-1)
-
-			for( int j=0; j< array.length-1-i;j++) {
+			isSwapped = false;
+			for( int j=0; j< array.length-1-i;j++) { // Time complexity O(n2) - order of "n" square
 				// This for loop is for comparisons.
 				
-				/* ""array.length-1-i"" ... here reason for subtracting "i". 
+				/* ""array.length-1-i"" ... here reason for subtracting "i" from "array.length-1". 
 				 * as we go along sorting the number, the number of elements 
 				 * which need to be sorted will be decreased with each pass.
 				 */
 			
 				if(array[j] > array[j+1]) {
 					// Swapping logic
+					isSwapped = true;
 					int temp = array[j];
 					array[j] = array[j+1];
 					array[j+1] = temp;
@@ -37,7 +38,17 @@ public class BubbleSort {
 				
 			}
 			
+			/* 
+			 * breaks the parent for loop if no swapping is done 
+			 * If no swapping is done that means the array has been sorted.
+			 */
+			if(isSwapped == false) break;
+			
 		}
+		
+		/*
+		 * finally Prints the sorted array.
+		 */
 		for(int x = 0; x < array.length; x++) { 
 			System.out.print(array[x]);
 		}
